2481b2967d325f3d6261c8a980939877e1024adc
jacob stivers <jacobstivers@jacobs-MacBook-Pro.local>
2017-10-25 23:11:25 -0500

    Assignment 7 progress...#7 incomplete

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -4,3 +4,18 @@ Elvendata1<-read.table("~/Documents/Graduate School/DataSciAg/Assignments/Assign
                        header = TRUE, sep = ",",
                        nrows = 11,
                        na.strings = c("-999", "-999.0"))
+#1-5 Complete
+#6
+
+library(stringr)
+GCcontent <- function (data,rownumber,column){
+  V1<-data$column
+  V2<- V1[rownumber]
+  answer<- ((str_count(V2,"G")+str_count(V2, "C"))/str_length(V2*100))
+  return(answer)
+}
+
+GCcontent(Elvendata1)
+GCcontent(Elvendata1, 1, dnaseq)
+
+#7 Incomplete
