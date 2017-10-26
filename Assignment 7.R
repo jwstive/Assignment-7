293b737b6e02f7a6ee135bdce8b4c68921aa4d74
jacob stivers <jacobstivers@jacobs-MacBook-Pro.local>
2017-10-25 22:11:54 -0500

    1. added Rstudio files to version control

---
+++ Assignment 7.R
@@ -0,0 +1,19 @@
+#Version Control
+#formal way of keeping track of changes
+
+
+
+
+
+
+
+
+
+
+
+
+
+#problem 5, When needing to email professor, 
+#go to github, open repository, 
+#there is a settings tab, and on left hand side youll see tab called collaborators
+#click on collab tab and use form below to add "palderman", add collaborator
