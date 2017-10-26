07248221039f0fe970995fa44512870bbbb9a7b1
jacob stivers <jacobstivers@jacobs-MacBook-Pro.local>
2017-10-25 22:32:47 -0500

    changed R code to adjust for Renamed data

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,6 +1,6 @@
 #Continuing your analyses of house-elves with Dr. Granger. 
 
-Elvendata1<-read.table("~/Documents/Graduate School/DataSciAg/Assignments/Assignment 7/Assignment 7/Data/houseelf_earlength_dna_data.csv",
+Elvendata1<-read.table("~/Documents/Graduate School/DataSciAg/Assignments/Assignment 7/Assignment 7/Data/houseelf_earlength_dna_data_1.csv",
                        header = TRUE, sep = ",",
                        nrows = 11,
                        na.strings = c("-999", "-999.0"))
